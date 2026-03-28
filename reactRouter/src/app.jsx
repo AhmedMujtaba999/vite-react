ab06b9e076678a0567e2b8311916c8611550d699
ahmed <mujtabaaliahmed559@gmail.com>
2026-03-28 21:44:26 +0530

    inbuild files

---
+++ reactRouter/src/app.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'preact/hooks'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import './app.css'
+import Home from './components/home/Home'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+    </>
+  )
+}
