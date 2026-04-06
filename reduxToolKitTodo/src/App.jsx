7d9d4f2782837857d8fb3df2cd5d755e37395af7
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-06 20:33:28 +0530

    imported components--> Addtodos, and Todos and used

--- reduxToolKitTodo/src/App.jsx
+++ reduxToolKitTodo/src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Addtodos from './components/Addtodos'
+import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <h1>Reduc Toolkit todo app</h1>
+    <h1 className="text-3xl font-bold text-white mb-4 mt-8">Redux Toolkit todo app</h1>
+    <Addtodos />
+    <Todos />
      </>
 
   )
