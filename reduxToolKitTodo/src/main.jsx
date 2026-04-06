f6428be1d3ff2326f8e741079c6b6072981138f0
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-06 20:34:24 +0530

    wrapped the whole App component with provider with having store in the place of value

--- reduxToolKitTodo/src/main.jsx
+++ reduxToolKitTodo/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux' //importing the Provider component from react-redux to provide the Redux store to the React application
+import { store } from './app/store' //importing the Redux store from the store file to be used in the Provider component
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <Provider store={store}>
+      <App />
+    </Provider>
+ 
 )
